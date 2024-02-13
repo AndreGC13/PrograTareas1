c52f82f8190cdafd035784b974a7d8458cb51681
AndreGC13 <156692851+AndreGC13@users.noreply.github.com>
2024-02-12 18:08:51 -0600

    Update Ejercicio 2.21.cpp

--- Capitulo 2/Ejercicio 2.21.cpp
+++ Capitulo 2/Ejercicio 2.21.cpp
@@ -2,7 +2,7 @@
 //Figuras geometricas dadas para poder realizar con forma de impresion 
 int main() {
 
-    std::cout << "Figura_Cuadrado:\endl";  
+    std::cout << "Figura_geometricas:\endl";  
     std::cout << "*********     *****                      * *             *         \endl";
     std::cout << "*       *    *     *                    *****          *    *       \endl";
     std::cout << "*       *   *       *                  *******        *      *      \endl";
