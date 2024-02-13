e82cfb65907ec49d0c9380d0be4f0d33b3db94f1
AndreGC13 <156692851+AndreGC13@users.noreply.github.com>
2024-02-12 18:04:42 -0600

    Update Ejercicio 2.21.cpp

--- Capitulo 2/Ejercicio 2.21.cpp
+++ Capitulo 2/Ejercicio 2.21.cpp
@@ -1,44 +1,16 @@
 #include <iostream>
-
+//Figuras geometricas dadas para poder realizar
 int main() {
 
-    std::cout << "Figura_Cuadrado:\n";
-    std::cout << "*********\n";
-    std::cout << "*       *\n";
-    std::cout << "*       *\n";
-    std::cout << "*       *\n";
-    std::cout << "*       *\n";
-    std::cout << "*       *\n";
-    std::cout << "*       *\n";
-    std::cout << "*********\n\n";
-
-
-    std::cout << "Figura_óvalo:\n";
-    std::cout << "  ***  \n";
-    std::cout << " *   * \n";
-    std::cout << "*     *\n";
-    std::cout << " *   * \n";
-    std::cout << "  ***  \n\n";
-
-
-    std::cout << "Figura_Flecha:\n";
-    std::cout << "    *   \n";
-    std::cout << "   ***  \n";
-    std::cout << "  ***** \n";
-    std::cout << " ******* \n";
-    std::cout << "    *   \n";
-    std::cout << "    *   \n";
-    std::cout << "    *   \n";
-    std::cout << "    *   \n\n";
-
-
-    std::cout << "Figura_Diamante:\n";
-    std::cout << "    *   \n";
-    std::cout << "   ***  \n";
-    std::cout << "  ***** \n";
-    std::cout << "  ***** \n";
-    std::cout << "   ***  \n";
-    std::cout << "    *   \n";
+    std::cout << "Figura_Cuadrado:\endl";  
+    std::cout << "*********     *****                      * *             *         \endl";
+    std::cout << "*       *    *     *                    *****          *    *       \endl";
+    std::cout << "*       *   *       *                  *******        *      *      \endl";
+    std::cout << "*       *  *         *                *********      *        *     \endl";
+    std::cout << "*       *  *         *               ***********    *          *    \endl";
+    std::cout << "*       *    *      *                     *          *        *       \endl";
+    std::cout << "*       *     *****                       *           *     *       \endl";
+    std::cout << "*********                               *****            *         \endl\endl";
 
     return 0;
 }
